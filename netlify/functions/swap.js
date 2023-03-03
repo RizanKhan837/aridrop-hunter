103819a1ca32e925cb25afe502c24a5bbc026288
rizankhan <rizankhan837@gmail.com>
2023-03-03 15:24:35 +0500

    Firsr Commit

---
+++ netlify/functions/swap.js
@@ -0,0 +1,36 @@
+const { schedule } = require("@netlify/functions");
+const { ethers } = require("ethers")
+require("dotenv").config()
+
+const IUniswapV2Router02 = require('@uniswap/v2-periphery/build/IUniswapV2Router02.json')
+
+const handler = async function (event, context) {
+  console.log("Received event:", event);
+
+  const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL)
+
+  const ACCOUNT_1 = new ethers.Wallet(process.env.PRIVATE_KEY_1, provider)
+
+  // Main Contract To Interact With When Swapping Tokens On Uniswap
+  const V2_ROUTER = new ethers.Contract(process.env.V2_ROUTER_ADDRESS, IUniswapV2Router02.abi)
+
+  const PATH = [process.env.WETH_ADDRESS, process.env.TOKEN_ADDRESS] // Path To Take When Swapping Tokens
+  const DEADLINE = Math.floor(Date.now() / 1000) + 60 * 10 // 10 minutes // Time Until Transaction Expires
+
+  const transaction = await V2_ROUTER.connect(ACCOUNT_1).swapExactETHForTokens( // Swap ETH For Tokens
+    0,
+    PATH,
+    ACCOUNT_1.address,
+    DEADLINE,
+    { value: process.env.SWAP_AMOUNT }
+  )
+
+  console.log(`Swap Complete!`)
+  console.log(`See transaction at: https://goerli.etherscan.io/tx/${transaction.hash}/`)
+
+  return {
+    statusCode: 200,
+  }
+}
+
+exports.handler = schedule("@daily", handler) // This allows us to schedule these tasks on netlify
